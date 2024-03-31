28827e55184e22ffee24d3f69dcf801f9be1bee7
Dotronghuy <140100028+Dotronghuy@users.noreply.github.com>
2024-03-31 22:53:37 +0700

    project1

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+  1. render slider
+  2. render category ( danh mục)  
+  3. render flash sale
+  4. render shoppe mall
+  5. render body  
+**/
+// tạo hiệu ứng trượt ở siler/ shopp mall / danh mục
+// xóa hover language-show ở fisrt child
+// tạo coutdown-time ở flash sale
+// render content flash sale
+// thêm class active-dot khi banner shoppmall/siler trượt 
